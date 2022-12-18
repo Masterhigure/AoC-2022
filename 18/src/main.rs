2bc6c0800ae66ca84b9f266cf68882971a81686a
Masterhigure <41048478+Masterhigure@users.noreply.github.com>
2022-12-18 11:42:36 +0100

    Added comments

--- 18/src/main.rs
+++ 18/src/main.rs
@@ -15,6 +15,12 @@ pub fn read_input(filename: &str) -> Input {
 }
 
 pub fn task_one(i: Input) -> i32 {
+    // The set sides contains _all_ sides of all cubes of the droplet (indexed by
+    // direction of face and the position of the cube that's to the
+    // left of / above / behind that face. And count counts
+    // the number of sides that have been added twice, which are all the
+    // shared sides. Subtract to get answer.
+
     let mut count = 0;
     let mut sides: HashSet<(i32, (i32, i32, i32))> = HashSet::new();
     for &c in &i {
